26c8106000277c5f52fb87ac1db1384844efeef2
Finn Pauls <derfinn+github@gmail.com>
2011-06-10 12:12:18 +0200

    english version

--- scriptfu_add_border.scm
+++ scriptfu_add_border.scm
@@ -1,20 +1,27 @@
+;Creative Commons Attribution Noncommercial Share Alike (cc-by-nc-sa), Finn Pauls <ich@finnpauls.de>
+;
+;This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+;To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to
+;Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
+
 (script-fu-register
-    "script-fu-add-border"
-    "Go Ahead! Border"
-    "Creates a two-colored border."
-    "Finn Pauls, finnpauls.de"
-    "This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA."
+    "script-fu-add-bicolored-frame"
+    "Add bicoloured Frame"
+    "Creates a two-colored frame."
+    "Finn Pauls <ich@finnpauls.de>"
+    "Creative Commons Attribution Noncommercial Share Alike (cc-by-nc-sa) Finn Pauls"
     "May 15, 2011"
     "RGB*"
-    SF-IMAGE    "Bild"  0
+    SF-IMAGE    "Image"  0
     SF-DRAWABLE "Drawable"  0
-    SF-COLOR  "Farbe des innerer Rahmens"         '(255 255 255)   ;inFirstBorderColor
-    SF-COLOR  "Farbe des zweiten Rahmens"         '(172 172 172)   ;inSecondBorderColor
-    SF-ADJUSTMENT  "Breite des inneren Rahmens"     '(2 1 500 1 1 0 1) ;ininFirstBorderSize
-    SF-ADJUSTMENT  "Breite des zweiten Rahmens"     '(1 1 500 1 1 0 1) ;ininSecondBorderSize
+    SF-COLOR  "Color of inner border"         '(255 255 255)   ;inFirstBorderColor
+    SF-COLOR  "Color of outer border"         '(172 172 172)   ;inSecondBorderColor
+    SF-ADJUSTMENT  "Width of inner border"     '(2 1 500 1 1 0 1) ;ininFirstBorderSize
+    SF-ADJUSTMENT  "Width of outer border"     '(1 1 500 1 1 0 1) ;ininSecondBorderSize
 )
-(script-fu-menu-register "script-fu-add-border" "<Image>/Filter/Decoration")
-(define (script-fu-goahead-border inImage inDrawable inFirstBorderColor inSecondBorderColor inFirstBorderSize inSecondBorderSize)
+(script-fu-menu-register "script-fu-add-bicolored-frame" "<Image>/Filters/Decor")
+(define (script-fu-add-bicolored-frame inImage inDrawable inFirstBorderColor inSecondBorderColor inFirstBorderSize inSecondBorderSize)
     (let*
         (
             (ImageWidth (car(gimp-image-width inImage)))
@@ -43,23 +50,21 @@
                            SecondBorderWidth
                            SecondBorderHeight
                            RGB-IMAGE
-                           "Go Ahead Rahmen"
+                           "Frame"
                            100
                            NORMAL
                           )
                       )
             )
         )
-        ;Make it Smaller
-        
-        
-        ;Add First Border
+       
+        ;Add Inner Border
         (gimp-selection-all inImage)
         (gimp-image-resize inImage FirstBorderWidth FirstBorderHeight inFirstBorderSize inFirstBorderSize)
         (gimp-context-set-foreground inFirstBorderColor)
         (gimp-image-add-layer inImage FirstBorderLayer 1)
         (gimp-drawable-fill FirstBorderLayer 0)
-        ;Add Second Border
+        ;Add Outer Border
         (gimp-selection-all inImage)
         (gimp-image-resize inImage SecondBorderWidth SecondBorderHeight inSecondBorderSize inSecondBorderSize)
         (gimp-context-set-foreground inSecondBorderColor)
@@ -67,9 +72,7 @@
         (gimp-drawable-fill SecondBorderLayer 0)
         ;Clean it up
         (gimp-selection-all inImage)
-        ;Save it (Inactive)
-        ;(gimp-file-save 0 inImage inDrawable "aktuell_xxxxxx.jpg" "aktuell_xxxxxx.jpg")
-        
+    
     )
 )
 
